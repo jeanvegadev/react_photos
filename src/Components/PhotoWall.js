2c3401143526a76073d22b511c84ac527034345d
jeanvegadev <jcvega.dev@gmail.com>
2022-09-06 23:53:46 -0500

    photo js

---
+++ src/Components/PhotoWall.js
@@ -0,0 +1,15 @@
+import {Component} from 'react';
+import Photo from './Photo';
+
+class PhotoWall extends Component {
+    render(){
+        return(
+            <div className='photoGrid'>
+                {this.props.posts.map(
+                    (post, index) => <Photo key={index} post={post}/>)}
+            </div>
+        )
+    }
+}
+
+export default PhotoWall
