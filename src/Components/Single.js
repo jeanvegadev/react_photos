32a11eddce045fb174cf2872f3f48e2476566848
jeanvegadev <jcvega.dev@gmail.com>
2022-09-27 22:49:41 -0500

    comments component created

--- src/Components/Single.js
+++ src/Components/Single.js
@@ -1,13 +1,19 @@
 import {Component} from 'react';
 import {useParams} from 'react-router-dom';
+import Photo from './Photo';
+import Comments from './Comments';
 
 function Single (props) {
 
         let params = useParams();
-        console.log(params);
+        const idf = parseInt(params.id);
+        console.log(idf);
+        const post = props.posts.find((posta)=>posta.id===idf);
+        console.log(post);
         return(
             <div className='single-photo'>
-                hola
+                <Photo post={post}/>
+                <Comments/>
             </div>
         )
 }
